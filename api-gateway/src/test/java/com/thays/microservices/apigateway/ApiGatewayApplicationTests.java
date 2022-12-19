61052506c6a1eccc39470dad5ba9aa72f372e12e
Thays Vieira <thaysvieira39@gmail.com>
2022-12-19 14:41:31 +0100

    first commit

---
+++ api-gateway/src/test/java/com/thays/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thays.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
